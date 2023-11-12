a14ec4d902419023f81624af66046cbd9f78afa0
regmibishal1 <regmibishal1@gmail.com>
2023-11-12 14:53:15 -0500

    Updated all the configs and switched over to using scss with ng material

--- src/app/Shared/navbar/navbar.component.ts
+++ src/app/Shared/navbar/navbar.component.ts
@@ -2,12 +2,10 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-navbar',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+	selector: 'app-navbar',
+	standalone: true,
+	imports: [CommonModule],
+	templateUrl: './navbar.component.html',
+	styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
-
-}
+export class NavbarComponent {}
