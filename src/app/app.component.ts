27ccd9bf6fc149c087ee5f2de49457c82f3442ba
regmibishal1 <regmibishal1@gmail.com>
2023-11-12 14:53:15 -0500

    Updated all the configs and switched over to using scss with ng material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './Shared/navbar/navbar.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	standalone: true,
+	imports: [CommonModule, RouterOutlet, NavbarComponent],
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Research Portfolio';
+	title = 'Research Portfolio';
 }
