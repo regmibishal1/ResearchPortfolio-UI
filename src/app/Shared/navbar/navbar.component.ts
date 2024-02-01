c6554bce847a57db788c8ef694a94a783800f0f8
regmibishal1 <regmibishal1@gmail.com>
2024-01-31 21:34:48 -0500

    home and about page added

--- src/app/Shared/navbar/navbar.component.ts
+++ src/app/Shared/navbar/navbar.component.ts
@@ -4,6 +4,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { RouterModule } from '@angular/router';
 
 @Component({
 	selector: 'app-navbar',
@@ -14,6 +15,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 		MatButtonModule,
 		MatIconModule,
 		MatSidenavModule,
+		RouterModule
 	],
 	templateUrl: './navbar.component.html',
 	styleUrl: './navbar.component.scss',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './Pages/dashboard/dashboard.component';
+import { AboutComponent } from './Pages/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', redirectTo: 'home', pathMatch: 'full' },
+	{ path: 'home', component: DashboardComponent },
+	{ path: 'about', component: AboutComponent }
+];
