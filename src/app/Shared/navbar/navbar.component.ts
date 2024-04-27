12f4f941fafb4d49a06584a97e918cc4991a7c39
Bishal Regmi <regmibishal1@gmail.com>
2024-04-27 15:53:20 -0400

    Feature/adding pages (#1)
    * home and about page added
    
    * added a build to the repo for pages
    
    * Moved the files to see if this helps
    
    * Trying to make sure the page works
    
    * Removing docs and just using gh pages lib
    
    * updated home page and added some new logos
    
    * Updated and added new pages. More auth work needs to be. All setup for local work
    
    * updating some deployemt stuff
    
    * trying to see if instatlling the deploying lib will help lol
    
    * few more configuration things
    
    * Let
    
    * Trying one last thing
    
    * Trying another one last thing
    
    * Trying again
    
    * Little bit more consistent beauty
    
    * Making things more consistent
    
    * updated authentication and improved some visuals
    
    * Minor themeing change
    
    * getting the ui to hit the api
    
    * Forgot a key setting
    
    * updating line to trigger deployment again"

--- src/app/Shared/navbar/navbar.component.ts
+++ src/app/Shared/navbar/navbar.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { RouterModule } from '@angular/router';
+import { AuthService } from '../../Pages/auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
 	selector: 'app-navbar',
@@ -14,8 +18,46 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 		MatButtonModule,
 		MatIconModule,
 		MatSidenavModule,
+		RouterModule
 	],
 	templateUrl: './navbar.component.html',
 	styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent implements OnInit, OnDestroy {
+	isAuthenticated = false;
+	private authSubscription!: Subscription;
+
+	constructor(private authService: AuthService, private _snackBar: MatSnackBar) {}
+
+	ngOnInit() {
+		this.authSubscription = this.authService.getAuthStatus().subscribe(
+		(isAuth) => {
+			this.isAuthenticated = isAuth;
+		}
+		);
+	}
+
+	onLogout() {
+		this.authService.logout().subscribe({
+			next: () => {
+				this.openSnackBar('Logged out successfully!');
+			},
+			error: (error: Error) => {
+				this.openSnackBar(error.message);
+			},
+		});
+	}
+
+
+	ngOnDestroy() {
+		if (this.authSubscription) {
+		this.authSubscription.unsubscribe();
+		}
+	}
+
+	openSnackBar(message: string) {
+		this._snackBar.open(message, 'Close', {
+			duration: 5000,
+		});
+	}
+}
