8aabeb351ed762d1569e02abb32a00c782edea67
Bishal Regmi <regmibishal1@gmail.com>
2026-03-02 20:00:13 -0500

    Fix CI: Remove package-lock.json and add provideRouter to failing component tests

--- src/app/pages/page-not-found/page-not-found.component.spec.ts
+++ src/app/pages/page-not-found/page-not-found.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { provideRouter } from '@angular/router'
 
 import { PageNotFoundComponent } from './page-not-found.component'
 
@@ -9,6 +10,7 @@ describe('PageNotFoundComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [PageNotFoundComponent],
+      providers: [provideRouter([])],
     }).compileComponents()
 
     fixture = TestBed.createComponent(PageNotFoundComponent)
